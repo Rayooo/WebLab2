c96658d6bfa4925ed3265e85a41dd42fc5921be2
Rayooo <chenxiaolei.ray@gmail.com>
2016-05-09 20:33:14 +0800

    管理员添加用户

--- addUserDeal.php
+++ addUserDeal.php
@@ -15,35 +15,50 @@ $address = $_REQUEST["address"];
 $zipcode = $_REQUEST["zipcode"];
 $enterYear = $_REQUEST["enterYear"];
 $classId = $_REQUEST["classId"];
+$imageURL = "";
 //还有个文件
-echo "AAA";
+//如果是图片则保存
 if ((($_FILES["image"]["type"] == "image/gif") || ($_FILES["image"]["type"] == "image/jpeg") || ($_FILES["image"]["type"] == "image/pjpeg"))){
-    echo "aa";
     if ($_FILES["image"]["error"] > 0) {
+        //跳到错误页面,还没做
         echo "Return Code: " . $_FILES["image"]["error"] . "<br />";
     }
     else {
-        echo "Upload: " . $_FILES["image"]["name"] . "<br />";
-        echo "Type: " . $_FILES["image"]["type"] . "<br />";
-        echo "Size: " . ($_FILES["image"]["size"] / 1024) . " Kb<br />";
-        echo "Temp file: " . $_FILES["image"]["tmp_name"] . "<br />";
+//        echo "Upload: " . $_FILES["image"]["name"] . "<br />";
+//        echo "Type: " . $_FILES["image"]["type"] . "<br />";
+//        echo "Size: " . ($_FILES["image"]["size"] / 1024) . " Kb<br />";
+//        echo "Temp file: " . $_FILES["image"]["tmp_name"] . "<br />";
+        //改文件名,新文件名为年月日加五位随机数
+        $upfileType = explode(".", $_FILES["image"]["name"]);
+        $now = date("YmdHis");
+        $randNum = rand(10000,99999);
+        $newName = $now.$randNum.".".end($upfileType);
+//        echo "newName:".$newName."<br>";
 
-        if (file_exists("upload/" . $_FILES["image"]["name"])) {
+        if (file_exists("upload/" . $newName)) {
             echo $_FILES["image"]["name"] . " already exists. ";
         }
         else {
+            $imageURL = "upload/" . $newName;
             move_uploaded_file($_FILES["image"]["tmp_name"],
-                "upload/" . $_FILES["image"]["name"]);
-            echo "Stored in: " . "upload/" . $_FILES["image"]["name"];
+                $imageURL);
+//            echo "Stored in: " . "upload/" .$newName;
         }
     }
 }
 
-$url = 'index.php';
-echo "<script language='javascript'>";
-echo "location.href='$url'";
-echo "</script>";
-
-
+include "connectDB.php";
+$sql = "INSERT INTO students (userName, userPassword, realName, cardNo, business, enterYear, classId, mobile, address, zipcode, image)".
+        "VALUES ('$userName','$password','$realName','$cardNo','$business','$enterYear',$classId,'$mobile','$address','$zipcode','$imageURL') ";
+if($connection->query($sql)===true){
+    $url = 'index.php';
+    echo "<script language='javascript'>";
+    echo "location.href='$url'";
+    echo "</script>";
+}
+else {
+    //错误处理
+    echo $sql;
+}
 
 ?>
